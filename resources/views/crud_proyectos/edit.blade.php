4a962543e5e40edfaf24126fd9268855916bb485
Merlin-Stack <emersonmuncue@gmail.com>
2024-10-22 16:43:00 -0500

    correcion de funcion de editar

--- resources/views/crud_proyectos/edit.blade.php
+++ resources/views/crud_proyectos/edit.blade.php
@@ -8,10 +8,31 @@
         @csrf
         @method('PUT')
         
-        <!-- Campos similares a create.blade.php -->
-        
-        <!-- Agrega aquí los campos para editar el proyecto -->
-        
+        <div class="form-group">
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre" id="nombre" class="form-control" value="{{ $proyecto->nombre }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="descripcion">Descripción</label>
+            <textarea name="descripcion" id="descripcion" class="form-control" required>{{ $proyecto->descripcion }}</textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="fecha_inicio">Fecha Inicio</label>
+            <input type="date" name="fecha_inicio" id="fecha_inicio" class="form-control" value="{{ $proyecto->fecha_inicio }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="fecha_final">Fecha Final</label>
+            <input type="date" name="fecha_final" id="fecha_final" class="form-control" value="{{ $proyecto->fecha_final }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="estado">Estado</label><br />
+            <input type="checkbox" name="estado" id="estado" {{ $proyecto->estado ? 'checked' : '' }}> Terminado
+        </div>
+
         <button type="submit" class="btn btn-success">Actualizar Proyecto</button>
     </form>
 
